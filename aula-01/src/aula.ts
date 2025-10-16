36c951b6d15d5707c2c022f5a879165aee8c6f5e
mflilian <mayhharamorais@gmail.com>
2025-10-16 18:25:15 -0300

    refactor: define Video interface and update video import

--- aula-01/src/aula.ts
+++ aula-01/src/aula.ts
@@ -1,3 +1,20 @@
-import videos from "../data/videos.json";
 
-console.log(videos)
+import videosJson from "../data/videos.json";
+
+export interface Video {
+  titulo: string;
+  canal: string;
+  canalVerificado: boolean;
+  duracaoSeg: number;
+  views: number;
+  like: number;
+  dislike: number;
+  categoria: string;
+  tags: string[];
+  descricao: string;
+  url: string;
+}
+
+const videos: Video[] = videosJson as Video[];
+
+console.log(videos);
